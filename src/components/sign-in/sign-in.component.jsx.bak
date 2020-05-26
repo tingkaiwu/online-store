afd132fea45d128ecc4dfbbd794f9f7ee435edc7
Ting Kai Wu <tingkaiwuu@gmail.com>
2020-05-26 10:55:48 -0400

    add email/password sign up/in authentication for firebase, google sign in and authentication

--- src/components/sign-in/sign-in.component.jsx.bak
+++ src/components/sign-in/sign-in.component.jsx.bak
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,17 +17,10 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = async event => {
+  handleSubmit = event => {
     event.preventDefault();
 
-    const { email, password } = this.state;
-
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
-    } catch (error) {
-      console.log(error);
-    }
+    this.setState({ email: '', password: '' });
   };
 
   handleChange = event => {
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config={
+    apiKey: "AIzaSyDAEZc1kqsemeJGSAtMIQHUUDAN7yNi1ug",
+    authDomain: "online-store-c2889.firebaseapp.com",
+    databaseURL: "https://online-store-c2889.firebaseio.com",
+    projectId: "online-store-c2889",
+    storageBucket: "online-store-c2889.appspot.com",
+    messagingSenderId: "184150331361",
+    appId: "1:184150331361:web:1c874230b298b78ba9cca0",
+    measurementId: "G-84EFCT18QG"
+}
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
